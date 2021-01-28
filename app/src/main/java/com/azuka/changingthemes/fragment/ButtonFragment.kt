7bc52df8306e3aad970cbcbdeb1e8d9972513e22
ivanaazuka <ivana.situmorang@amarbank.co.id>
2021-01-28 14:14:05 +0700

    feat(button) : add enable/disable button

--- app/src/main/java/com/azuka/changingthemes/fragment/ButtonFragment.kt
+++ app/src/main/java/com/azuka/changingthemes/fragment/ButtonFragment.kt
@@ -1,8 +1,12 @@
 package com.azuka.changingthemes.fragment
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import com.azuka.changingthemes.R
+import com.azuka.changingthemes.databinding.ActivityMainBinding
+import com.azuka.changingthemes.databinding.FragmentButtonBinding
 
 
 /**
@@ -11,9 +15,40 @@ import com.azuka.changingthemes.R
  */
 
 class ButtonFragment : BaseFragment() {
+
+    private var _binding: FragmentButtonBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    private var isButtonEnable = true
+
     override val viewLayout: Int = R.layout.fragment_button
 
-    override fun onFragmentReady(view: View, savedInstanceState: Bundle?) {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentButtonBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
+    override fun onFragmentReady(view: View, savedInstanceState: Bundle?) {
+        binding.btnReset.setOnClickListener {
+            binding.btnTest.isEnabled = true
+        }
+
+        binding.btnTest.setOnClickListener {
+            binding.btnTest.apply {
+                isEnabled = !isButtonEnable
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
